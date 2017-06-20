abb050509208510c8a5c667954294cc60ff008c3
nmsutton <Nate Sutton>
2017-06-20 15:01:00 -0400

    created window

--- main.cpp
+++ main.cpp
@@ -64,5 +64,64 @@ int main()
 
    printf("%ld %ld %ld\n",red_mask>> 16, green_mask>>8, blue_mask);
 
+    Display                 *display2;
+    Visual                  *visual;
+    int                     depth;
+    int                     text_x;
+    int                     text_y;
+    XSetWindowAttributes    frame_attributes;
+    Window                  frame_window;
+    XFontStruct             *fontinfo;
+    XGCValues               gr_values;
+    GC                      graphical_context;
+    XKeyEvent               event;
+    char                    hello_string[] = "Hello World";
+    int                     hello_string_length = strlen(hello_string);
+
+    display2 = XOpenDisplay(NULL);
+    visual = DefaultVisual(display2, 0);
+    depth  = DefaultDepth(display2, 0);
+
+    frame_attributes.background_pixel = XWhitePixel(display2, 0);
+
+    /* create the application window */
+    frame_window = XCreateWindow(display2, XRootWindow(display2, 0),
+                                 0, 0, 400, 400, 5, depth,
+                                 InputOutput, visual, CWBackPixel,
+                                 &frame_attributes);
+    XStoreName(display2, frame_window, "Hello World Example");
+
+    XSelectInput(display2, frame_window, ExposureMask | StructureNotifyMask);
+    fontinfo = XLoadQueryFont(display2, "10x20");
+    gr_values.font = fontinfo->fid;
+    gr_values.foreground = XBlackPixel(display2, 0);
+    graphical_context = XCreateGC(display2, frame_window,
+                                  GCFont+GCForeground, &gr_values);
+    XMapWindow(display2, frame_window);
+
+    while ( 1 ) {
+        XNextEvent(display2, (XEvent *)&event);
+        switch ( event.type ) {
+            case Expose:
+            {
+                XWindowAttributes window_attributes;
+                int font_direction, font_ascent, font_descent;
+                XCharStruct text_structure;
+                XTextExtents(fontinfo, hello_string, hello_string_length,
+                             &font_direction, &font_ascent, &font_descent,
+                             &text_structure);
+                XGetWindowAttributes(display2, frame_window, &window_attributes);
+                text_x = (window_attributes.width - text_structure.width)/2;
+                text_y = (window_attributes.height -
+                          (text_structure.ascent+text_structure.descent))/2;
+                XDrawString(display2, frame_window, graphical_context,
+                            text_x, text_y, hello_string, hello_string_length);
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
    return 0;
 }
